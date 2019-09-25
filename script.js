3cf42988fad1304899292bd886fc61214895dd26
Son, Joon <Joon.Son@nfl.com>
2019-09-25 13:29:10 -0700

    do bar math

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var oneRepMax = function() {
 };
 
 var percentageify = function(value, percentage) {
-  return {percent: percentage, value: (value * percentage) / 100};
+  return { percent: percentage, value: (value * percentage) / 100 };
 };
 
 var fakeMax = function(oneRepMax) {
@@ -14,25 +14,27 @@ var percentages = function(oneRepMax) {
   var percents = [40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95];
   var pairs = [];
   for (let index in percents) {
+    let weight = percentageify(fakeMax(oneRepMax).value, percents[index]).value;
+    let weight45 = Math.max((weight - 45) / 2, 0);
+    let weight35 = Math.max((weight - 35) / 2, 0);
     pairs.push({
       percent: percents[index],
-      value: percentageify(
-        fakeMax(oneRepMax).value,
-        percents[index],
-      ).value.toFixed(2),
+      value: weight.toFixed(2),
+      weight45: weight45.toFixed(2),
+      weight35: weight35.toFixed(2)
     });
   }
   return pairs;
 };
 
 var updateUrl = function(event) {
-  window.history.pushState('', '', '#' + this.oneRepMax);
+  window.history.pushState("", "", "#" + this.oneRepMax);
 };
 
 var app = new Vue({
-  el: 'section',
+  el: "section",
   data: {
-    oneRepMax: oneRepMax(),
+    oneRepMax: oneRepMax()
   },
   computed: {
     fakeMax: function() {
@@ -40,9 +42,9 @@ var app = new Vue({
     },
     percentages: function() {
       return percentages(this.oneRepMax);
-    },
+    }
   },
   methods: {
-    updateUrl: updateUrl,
-  },
+    updateUrl: updateUrl
+  }
 });
